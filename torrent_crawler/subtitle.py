b24df34f539faa2388e39e44a645518e1951c864
FastFingertips <fastfingertips@gmail.com>
2026-02-22 22:51:40 +0300

    refactor: remove deprecated subtitle.py file

--- torrent_crawler/subtitle.py
+++
@@ -1,5 +0,0 @@
-from torrent_crawler.services.subtitle_service import SubtitleService
-
-class Subtitle(SubtitleService):
-    """Deprecated: Use SubtitleService from services package instead."""
-    pass
