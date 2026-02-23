5a0215de8ea6c5a984b880bd26215129680aeef8
FastFingertips <fastfingertips@gmail.com>
2026-02-23 08:30:00 +0300

    refactor: modernize data models with dataclasses (Python 3.7+), enforcing strong typing and reducing boilerplate code

--- torrent_crawler/core/models.py
+++ torrent_crawler/core/models.py
@@ -1,78 +1,87 @@
 import json
+from dataclasses import dataclass, field, asdict
+from typing import List, Dict, Optional
 from torrent_crawler.core.constants import Constants
 
 
+@dataclass
 class Ratings:
-    def __init__(self, ratings):
-        self.rotten_tomatoes_critics = ratings.get(Constants.rotten_tomatoes_critics_rating, '')
-        self.rotten_tomatoes_audience = ratings.get(Constants.rotten_tomatoes_audience_rating, '')
-        self.imdb = ratings.get(Constants.imdb_rating, '')
+    rotten_tomatoes_critics: str = ''
+    rotten_tomatoes_audience: str = ''
+    imdb: str = ''
+
+    @classmethod
+    def from_dict(cls, ratings: dict):
+        return cls(
+            rotten_tomatoes_critics=ratings.get(Constants.rotten_tomatoes_critics_rating, ''),
+            rotten_tomatoes_audience=ratings.get(Constants.rotten_tomatoes_audience_rating, ''),
+            imdb=ratings.get(Constants.imdb_rating, '')
+        )
 
 
+@dataclass
 class Torrents:
-    def __init__(self, torrents):
-        self.br3d = torrents[Constants.blu_ray_3d] if Constants.blu_ray_3d in torrents else None
-        self.br1080 = torrents[Constants.blu_ray_1080p] if Constants.blu_ray_1080p in torrents else None
-        self.br720 = torrents[Constants.blu_ray_720p] if Constants.blu_ray_720p in torrents else None
-        self.web1080 = torrents[Constants.web_1080p] if Constants.web_1080p in torrents else None
-        self.web720 = torrents[Constants.web_720p] if Constants.web_720p in torrents else None
+    br3d: Optional[str] = None
+    br1080: Optional[str] = None
+    br720: Optional[str] = None
+    web1080: Optional[str] = None
+    web720: Optional[str] = None
+
+    @classmethod
+    def from_dict(cls, torrents: dict):
+        return cls(
+            br3d=torrents.get(Constants.blu_ray_3d),
+            br1080=torrents.get(Constants.blu_ray_1080p),
+            br720=torrents.get(Constants.blu_ray_720p),
+            web1080=torrents.get(Constants.web_1080p),
+            web720=torrents.get(Constants.web_720p)
+        )
 
 
+@dataclass
 class Movie:
-    def __init__(self, movie_id, name, link, year):
-        self.id = movie_id
-        self.name = name
-        self.link = link
-        self.year = year
-        self.torrents = None
-        self.ratings = None
-        self.subtitle_url = ''
-        self.synopsis = ''
-        self.trailer = ''
-        self.screenshots = []
-        self.genres = []
-        self.likes = 0
-        self.runtime = ''
-        self.image = ''
-        self.similar_movies = []
+    id: int
+    name: str
+    link: str
+    year: int
+    torrents: Optional[Torrents] = None
+    ratings: Optional[Ratings] = None
+    subtitle_url: str = ''
+    synopsis: str = ''
+    trailer: str = ''
+    screenshots: List[str] = field(default_factory=list)
+    genres: List[str] = field(default_factory=list)
+    likes: int = 0
+    runtime: str = ''
+    image: str = ''
+    similar_movies: List[dict] = field(default_factory=list)
+    raw_torrents: Dict = field(default_factory=dict)
 
     def set_torrents(self, torrent_list):
-        self.torrents = Torrents(torrent_list)
+        self.torrents = Torrents.from_dict(torrent_list)
 
     def set_ratings(self, ratings):
-        self.ratings = Ratings(ratings)
+        self.ratings = Ratings.from_dict(ratings)
 
     def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'year': self.year,
-            'link': self.link,
-            'torrents': self.torrents.__dict__ if self.torrents else {},
-            'ratings': self.ratings.__dict__ if self.ratings else {},
-            'synopsis': self.synopsis,
-            'genres': self.genres,
-            'likes': self.likes,
-            'image': self.image,
-            'trailer': self.trailer,
-            'screenshots': self.screenshots,
-            'similar_movies': self.similar_movies
-        }
+        return asdict(self)
 
     def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+        return json.dumps(self.to_dict(), sort_keys=True, indent=4)
 
 
+@dataclass
 class SearchQuery:
-    def __init__(self, search_term, quality, genre, rating, order_by, year=0, language='en'):
-        self.search_term = search_term
-        self.quality = quality
-        self.genre = genre
-        self.rating = rating
-        self.order_by = order_by
-        self.language = language
-        self.year = year
+    search_term: str
+    quality: str
+    genre: str
+    rating: str
+    order_by: str
+    year: int = 0
+    language: str = 'en'
 
     def get_url(self):
-        return Constants.search_url.format(self.search_term, self.quality, self.genre,
-                                           self.rating, self.order_by, self.year, self.language)
+        return Constants.search_url.format(
+            self.search_term, self.quality, self.genre,
+            self.rating, self.order_by, self.year, self.language
+        )
